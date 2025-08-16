415f3a0934b6a1c4a179418f1ff4b4c2eaa5af87
Jhoc <johnharold.cruda@gmail.com>
2025-08-16 15:26:30 +0800

    Added another landing

---
+++ index.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Genspark Digital</h1>
+    <p>Welcome to Genspark Digital, your number one source for all things digital.</p>
+    <p>We offer a wide range of services to help you succeed in the digital world.</p>
+</body>
+</html>
